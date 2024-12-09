0eb784918098467031c8fc70c246b6b3d3932484
zachattack531 <26945971+zachattack531@users.noreply.github.com>
2024-12-08 22:57:19 -0500

    The image container is mostly done

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,4 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles/*");
     eleventyConfig.addPassthroughCopy("images/*");
     eleventyConfig.addPassthroughCopy("scripts/*");
-}
+};
